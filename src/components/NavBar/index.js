d410e35560b9a3b2c268f690835f8c9a685438b8
kevcast95 <kevcast06@gmail.com>
2022-04-08 05:05:46 -0500

    desktop done

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -10,7 +10,9 @@ function NavBar() {
     console.log(location);
     const navigate = useNavigate()
     const [tabSelected, setTabSelected] = useState(0)
-    const menuOptions = ['location','layer','menu']
+    const icons = ['location','layer','menu']
+    const menuOptiond = ['Ver Mapa', 'Lista de Casas', 'Acerca de']
+
 
     useEffect(()=>{
         const located = location.pathname
@@ -21,30 +23,41 @@ function NavBar() {
     },[location])
 
     const handleNavigation = (index) => {
+        if (index === 2){
+            alert('Por favor selecciones una casa de la lista')
+            return
+        }
         setTabSelected(index)
         if (index === 0) navigate('/')
         if (index === 1) navigate('/houses_list')
-        if (index === 2) alert('Por favor selecciones una casa de la lista')
     }
 
     return (
         <nav className='nav-bar'> 
-            <img src={logo} alt='draxo-logo' className='nav-bar_logo'/>
+            <span className="nav-bar_logo-container">
+                <img src={logo} alt='draxo-logo' className='nav-bar_logo'/>
+            </span>
             <ul className='nav-bar_menu'>
                 {
-                    menuOptions.map((opt, index) => (
-                        <li 
-                            className={tabSelected === index ?
+                    icons.map((opt, index) => (
+                        <li onClick={() => handleNavigation(index)}>
+                           <span
+                             className={tabSelected === index ?
                                 'nav-bar_items nav-bar_selected': 'nav-bar_items'} 
-                            key={index} 
-                            onClick={() => handleNavigation(index)}
-                        >
-                            <Icon
-                                item={opt}
-                                width="20px"
-                                height="20px"
-                                className="product-card__favorite-btn__icon"
-                            />
+                                key={index} 
+                            >
+                                <Icon
+                                    item={opt}
+                                    width="20px"
+                                    height="20px"
+                                    className="product-card__favorite-btn__icon"
+                                />
+                           </span>
+                           <p className={tabSelected === index ?
+                                "nav-bar_options options_selected":'nav-bar_options'}
+                            >
+                                {menuOptiond[index]}
+                            </p>
                         </li>
                     ))
                 }
